f0687eead7d02a5cc8d89e0cff61eec8588c4293
NXCThang <nxcthang1205@gmail.com>
2021-09-12 23:55:24 +0700

    PC

---
+++ BTB3/src/PC/CPU.java
@@ -0,0 +1,17 @@
+package PC;
+
+public class CPU {
+	private String name;
+	private ID id;
+	
+	public CPU(String name, ID id) {
+		this.name = name;
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public ID getID() {
+		return id;
+	}
+}
---
+++ BTB3/src/PC/Ram.java
@@ -0,0 +1,17 @@
+package PC;
+
+public class Ram {
+	private String name;
+	private ID id;
+	
+	public Ram(String name, ID id) {
+		this.name = name;
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public ID getID() {
+		return id;
+	}
+}
