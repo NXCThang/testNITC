48915588a7f6ec30c702afcf6963ed7a98d62e56
NXCThang <nxcthang1205@gmail.com>
2021-09-13 11:07:13 +0700

    PC
    This reverts commit 1291b3f934e7859171d67efa13035a12bcc83402.

---
+++ BTB3/src/PC/ID.java
@@ -0,0 +1,19 @@
+package PC;
+
+public class ID {
+	
+	private int id;
+	
+	public ID(int id) {
+		this.id = id;
+	}
+	
+	public int getID() {
+		return this.id;
+	}
+	
+	public void setID(int id) {
+		this.id = id;
+	}
+	
+}
